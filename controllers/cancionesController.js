63965df5d8233d346393c8cfffcaa37167e64ca5
FranAlamo <alamofrancis@gmail.com>
2023-05-14 20:19:00 -0300

    cambios rutas

--- controllers/cancionesController.js
+++ controllers/cancionesController.js
@@ -2,44 +2,61 @@ const knex = require("../config/knexfile");
 
 //cupido musical
 exports.listaCanciones = async (req, res) => {
-    try {
-        console.log(req.user)
-        const resultado = await knex.select("artista").from("musica");
-        res.status(200).json({ musica: resultado });
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-}
+  try {
+    console.log(req.user);
+    const resultado = await knex.select("artista").from("musica");
+    res.status(200).json({ musica: resultado });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 //top 20
 exports.top20 = async (req, res) => {
-    try {
-        const resultado = await knex.select("nombre", "artista").from("musica").limit(20);
-        res.status(200).json({ musica: resultado });
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-}
+  try {
+    const resultado = await knex
+      .select("nombre", "artista", "imagen")
+      .from("musica")
+      .limit(20);
+    res.status(200).json({ musica: resultado });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 //perfil
 exports.perfil = async (req, res) => {
-    const id = +req.params.id;
-    try {
-        const usuario = await knex.select('nombre_usuario', 'email')
-  .from('usuario')
-  .where({ id });
-        res.status(200).json({ usuario: usuario });
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-}
+  const id = +req.params.id;
+  try {
+    const usuario = await knex
+      .select("nombre_usuario", "email")
+      .from("usuario")
+      .where({ id });
+    res.status(200).json({ usuario: usuario });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 //buscar por genero
 exports.filtroGenero = async (req, res) => {
-    const nombre_genero = req.params.nombre_genero;
-    try {
-        const resultado = await knex.select('nombre', 'artista', 'nombre_genero').from('musica').join('genero', 'musica.id_genero', '=', 'genero.id')
-        .where({nombre_genero});
-        res.status(200).json({ musica: resultado });
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-}
+  const nombre_genero = req.params.nombre_genero;
+  try {
+    const resultado = await knex
+      .select("nombre", "artista", "nombre_genero")
+      .from("musica")
+      .join("genero", "musica.id_genero", "=", "genero.id")
+      .where({ nombre_genero });
+    res.status(200).json({ musica: resultado });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// lista todos los generos que existen
+exports.listaGeneros = async (req, res) => {
+  try {
+    const resultado = await knex.select("id", "nombre_genero").from("genero");
+    res.status(200).json({ generos: resultado });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- routes/cancionesRoutes.js
+++ routes/cancionesRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { listaCanciones, top20, perfil, filtroGenero } = require("../controllers/cancionesController");
+const {
+  listaCanciones,
+  top20,
+  perfil,
+  filtroGenero,
+  listaGeneros,
+} = require("../controllers/cancionesController");
 const { verifyToken } = require("../middlewares/auth/auth");
 const { runValidation } = require("../middlewares/validator");
 const routes = express.Router();
@@ -7,8 +13,12 @@ const routes = express.Router();
 routes.get("/canciones", verifyToken, runValidation, listaCanciones);
 routes.get("/top20", verifyToken, runValidation, top20);
 routes.get("/perfil", verifyToken, runValidation, perfil);
-routes.get("/porGenero/:nombre_genero", verifyToken, runValidation, filtroGenero);
-
-
+routes.get(
+  "/porGenero/:nombre_genero",
+  verifyToken,
+  runValidation,
+  filtroGenero
+);
+routes.get("/generos", verifyToken, runValidation, listaGeneros);
 
 module.exports = routes;
