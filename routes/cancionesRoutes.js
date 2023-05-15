4e0460bbd79dd7bd390941234988b58d762f2d4b
Deividds1 <deivid1421@gmail.com>
2023-05-15 20:47:57 -0300

    ruta para cupido

--- routes/cancionesRoutes.js
+++ routes/cancionesRoutes.js
@@ -5,6 +5,7 @@ const {
   perfil,
   filtroGenero,
   listaGeneros,
+  filtroArtista,
 } = require("../controllers/cancionesController");
 const { verifyToken } = require("../middlewares/auth/auth");
 const { runValidation } = require("../middlewares/validator");
@@ -14,11 +15,18 @@ routes.get("/canciones", verifyToken, runValidation, listaCanciones);
 routes.get("/top20", verifyToken, runValidation, top20);
 routes.get("/perfil", verifyToken, runValidation, perfil);
 routes.get(
-  "/porGenero/:nombre_genero",
+  "/porGenero/",
   verifyToken,
   runValidation,
   filtroGenero
 );
+routes.get(
+  "/porArtista/",
+  verifyToken,
+  runValidation,
+  filtroArtista
+);
+
 routes.get("/generos", verifyToken, runValidation, listaGeneros);
 
 module.exports = routes;
